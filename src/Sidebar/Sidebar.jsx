cb6a1b6d31742294dc79177379a175952006f4fe
Anuj mishra <captainanuj2004@gmail.com>
2024-09-03 15:51:03 +0530

    added-state-changes

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React,{useState} from "react";
 import "./Sidebar.css"
 import assets from "../assets/assets.js";
 function Sidebar(){
-    return(<div className="sidebar">
+    const [extend,setextend]=useState(false);
+    function handleClick(){
+       setextend(prevExtend=>{
+        return !prevExtend;
+       })
+        };
+        return(<div className="sidebar">
         <div className="uppercomponent">
-        <div className="menu">
-        <img className="menu-img" src={assets.menu_icon} alt="menu-icon"/>
+           <div className="menu">
+        <img onClick={handleClick}className="menu-img" src={assets.menu_icon} alt="menu-icon"/>
         </div>
+        {extend==true?<div>
         <button className="newchat"><img className="plus-img" src={assets.plus_icon} alt="plus-icon"/><p>New chat</p></button>
         <p className="recent">Recent</p>
-        <div className="recent-chat">
+        </div>:null}
+        
+        {extend==true?<div className="recent-chat">
             <img className="message-img" src={assets.message_icon} alt="message-icon"/>
-            <p>What is react...</p></div>
+            <p>What is react...</p></div>:null}
         </div>
-       <div className="lowercomponent">
+       {extend==true?<div className="lowercomponent">
         <div className="help">
         <img className="history-img" src={assets.question_icon} alt="history-icon"/>
         <p>Help</p>
@@ -27,8 +36,7 @@ function Sidebar(){
         <p>Settings</p>
         </div>
    
-    </div>
-        
-    </div>)
+    </div>:null}
+       </div> )
 }
 export default Sidebar;
